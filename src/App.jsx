e9954c1bfc7a02d7b06ac7887f5f3f663d558ff9
Hector <baseballhector@gmail.com>
2023-09-21 19:49:46 -0400

    Importing components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import GamesContainer from "./components/GamesContainer/GamesContainer";
+
+import styles from "./App.module.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <Navbar />
+      <GamesContainer />
+    </div>
+  );
 }
 
 export default App;
