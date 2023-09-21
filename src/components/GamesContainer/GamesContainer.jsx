35806b4b099ae38cf0648c879da21686683ce849
Hector <baseballhector@gmail.com>
2023-09-21 19:48:35 -0400

    Stylings for games container

---
+++ src/components/GamesContainer/GamesContainer.jsx
@@ -0,0 +1,7 @@
+import styles from "./GamesContainer.module.css";
+
+const GamesContainer = () => {
+  return <div className={styles["games-container"]}></div>;
+};
+
+export default GamesContainer;
