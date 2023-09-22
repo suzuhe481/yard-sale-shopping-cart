a8dd404a711f04ff588fcbafc684d88e62840211
Hector <baseballhector@gmail.com>
2023-09-22 18:20:13 -0400

    Links to cart and home. Shopping cart icon. Dummy counter for cart.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <p className={styles.title}>Yard Sale</p>
-      <p className={styles.cart}>Cart</p>
+      <Link to="/" className={styles.title}>
+        Yard Sale
+      </Link>
+      <Link to="/cart" className={styles.cart}>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <div className={styles.counter}>123</div>
+      </Link>
     </div>
   );
 };
