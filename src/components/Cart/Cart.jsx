2847c1c58a841f0c00b10573296b8330da146b99
Hector <baseballhector@gmail.com>
2023-09-22 18:11:43 -0400

    Initial cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
+function Cart() {
+  return (
+    <div>
+      <Navbar />
+      <div>The shopping cart</div>
+    </div>
+  );
+}
+
+export default Cart;
