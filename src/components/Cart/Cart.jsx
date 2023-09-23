21f678c2d61a6b4cb3d5a870069a06a38833c5ed
Hector <baseballhector@gmail.com>
2023-09-22 20:23:37 -0400

    Initial Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import Navbar from "../Navbar/Navbar";
 
+import styles from "./Cart.module.css";
+
 function Cart() {
   return (
-    <div>
+    <div className={styles.cart}>
       <Navbar />
       <div>The shopping cart</div>
     </div>
