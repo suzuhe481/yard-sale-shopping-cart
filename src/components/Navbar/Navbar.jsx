8f8054f476e55d3d493dd03945e0e643c6d1fb25
Hector <baseballhector@gmail.com>
2023-09-29 19:33:08 -0400

    Using cartAmountOfItems from Context. Hides cart counter if 0 items in cart.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../CartProvider";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { cartAmountOfItems } = useContext(CartContext);
+
   return (
     <div className={styles.navbar}>
       <Link to="/" className={styles.title}>
@@ -10,7 +14,9 @@ const Navbar = () => {
       </Link>
       <Link to="/cart" className={styles.cart}>
         <i className="fa-solid fa-cart-shopping"></i>
-        {/* <div className={styles.counter}>123</div> */}
+        {cartAmountOfItems > 0 && (
+          <div className={styles.counter}>{cartAmountOfItems}</div>
+        )}
       </Link>
     </div>
   );
