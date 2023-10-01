be9aa404365b50f513d6b566f9dc868ba45fac81
Hector <baseballhector@gmail.com>
2023-10-01 17:42:45 -0400

    Button element to close modal

--- src/components/UI/ItemAddedModal/ItemAddedModal.jsx
+++ src/components/UI/ItemAddedModal/ItemAddedModal.jsx
@@ -1,11 +1,27 @@
+import PropTypes from "prop-types";
+
 import styles from "./ItemAddedModal.module.css";
 
-const ItemAddedModal = () => {
+const ItemAddedModal = (props) => {
   return (
     <div className={styles["modal-container"]}>
+      <div className={styles["button-container"]}>
+        <button
+          className={styles["close-button"]}
+          onClick={() => {
+            props.setItemAdded(false);
+          }}
+        >
+          <i className="fa-solid fa-x"></i>
+        </button>
+      </div>
       <div className={styles["modal-message"]}>Item added successfully</div>
     </div>
   );
+};
+
+ItemAddedModal.propTypes = {
+  setItemAdded: PropTypes.func,
 };
 
 export default ItemAddedModal;
