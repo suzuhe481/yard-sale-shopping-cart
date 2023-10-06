7efcd43b979be17ac4adb464977953bb143cac2a
Hector <baseballhector@gmail.com>
2023-10-06 18:30:28 -0400

    Smoothly scroll to top on click.

--- src/components/UI/ScrollToTopButton/ScrollToTopButton.jsx
+++ src/components/UI/ScrollToTopButton/ScrollToTopButton.jsx
@@ -5,10 +5,16 @@ import styles from "./ScrollToTopButton.module.css";
 const ScrollToTopButton = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
 
+  // Handles the position of the window.
   const handleScroll = () => {
     const position = window.scrollY;
 
     setScrollPosition(position);
+  };
+
+  // Scrolls to top of screen smoothly.
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   var isVisible = false;
@@ -26,7 +32,7 @@ const ScrollToTopButton = () => {
     <>
       {isVisible && (
         <div className={styles["button-container"]}>
-          <button>
+          <button onClick={scrollToTop}>
             <i className="fa-solid fa-arrow-up"></i>
           </button>
         </div>
